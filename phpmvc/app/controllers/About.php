cd18ab5d5d6b63ad9a9da4ac6dc763d4193323e4
nadliw090801 <muhammadwildan334@gmail.com>
2021-09-30 13:13:12 +0700

    Mempelajari View pada aplikasi MVC

--- phpmvc/app/controllers/About.php
+++ phpmvc/app/controllers/About.php
@@ -1,14 +1,23 @@
 <?php
 
-class About {
+class About extends Controller {
 
-  public function index($nama = 'Wildhan', $pekerjaan = 'Mahasiswa')
+  public function index($nama = 'Wildhan', $pekerjaan = 'Mahasiswa', $umur = 20)
   {
-    echo "Halo, nama saya  $nama, saya adalah seorang $pekerjaan";
+    $data['nama'] = $nama;
+    $data['pekerjaan'] = $pekerjaan;
+    $data['umur'] = $umur;
+    $data['judul'] = 'About Me';
+    $this->view('templates/header', $data);
+    $this->view('about/index', $data);
+    $this->view('templates/footer');
   }
 
   public function page()
   {
-    echo 'About/page';
+    $data['judul'] = 'Pages';
+    $this->view('templates/header', $data);
+    $this->view('about/page');
+    $this->view('templates/footer');
   }
 }
